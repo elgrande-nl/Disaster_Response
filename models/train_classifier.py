27daed339f4fa8fd07bf33ad50d29d189494d4e4
sander <47451434+elgrande-nl@users.noreply.github.com>
2023-01-03 17:45:47 +0100

    template

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,8 +1,71 @@
-In a Python script, train_classifier.py, write a machine learning pipeline that:
+# In a Python script, train_classifier.py, write a machine learning pipeline that:
 
-Loads data from the SQLite database
-Splits the dataset into training and test sets
-Builds a text processing and machine learning pipeline
-Trains and tunes a model using GridSearchCV
-Outputs results on the test set
-Exports the final model as a pickle file
+# Loads data from the SQLite database
+# Splits the dataset into training and test sets
+# Builds a text processing and machine learning pipeline
+# Trains and tunes a model using GridSearchCV
+# Outputs results on the test set
+# Exports the final model as a pickle file
+
+
+import logging
+import sys
+import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
+
+logging.basicConfig(level=logging.INFO)
+
+
+def load_data(database_filepath):
+    pass
+
+
+def tokenize(text):
+    pass
+
+
+def build_model():
+    pass
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    pass
+
+
+def save_model(model, model_filepath):
+    pass
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print("Loading data...\n    DATABASE: {}".format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+        print("Building model...")
+        model = build_model()
+
+        print("Training model...")
+        model.fit(X_train, Y_train)
+
+        print("Evaluating model...")
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print("Saving model...\n    MODEL: {}".format(model_filepath))
+        save_model(model, model_filepath)
+
+        print("Trained model saved!")
+
+    else:
+        print(
+            "Please provide the filepath of the disaster messages database "
+            "as the first argument and the filepath of the pickle file to "
+            "save the model to as the second argument. \n\nExample: python "
+            "train_classifier.py ../data/DisasterResponse.db classifier.pkl"
+        )
+
+
+if __name__ == "__main__":
+    main()
