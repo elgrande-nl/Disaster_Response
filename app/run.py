e6e813d74a7cf23d444c942576cb1a71a7981c27
sander <47451434+elgrande-nl@users.noreply.github.com>
2023-01-03 11:32:13 +0100

    Update run.py

---
+++ app/run.py
@@ -0,0 +1,5 @@
+ Flask Web App
+We are providing much of the flask web app for you, but feel free to add extra features depending on your knowledge of flask, html, css and javascript. For this part, you'll need to:
+
+Modify file paths for database and model as needed
+Add data visualizations using Plotly in the web app. One example is provided for you
